983f4e42a66c090aab2e54e65b45b5aeb2ea62ec
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 23:18:35 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,9 @@ require "pry"
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
- 
+
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
+
+create_project_hash
