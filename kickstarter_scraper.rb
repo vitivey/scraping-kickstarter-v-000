1dfd6dd1c6e842308a6989ade55db1419ef1e488
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 23:53:35 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,7 @@ def create_project_hash
     projects[title.to_sym]={
       image_link: project.css(".project-thumbnail a img").attribute("src").value,
       description: project.css("p.bbcard_blurb").text,
-      location: project.css(".project-meta .location-name").text,
+      location: project.css(".project-meta span.location-name").text,
       percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
     end
