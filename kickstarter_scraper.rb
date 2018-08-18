dcf7dfad43e576c4666e04a813db8ae7ec6cd278
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 23:45:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,16 +8,15 @@ def create_project_hash
   projects={}
 
   page_section = kickstarter.css("li.project.grid_4")
-  
-  page_section.each do |project|
-    title=kickstarter.css(".project .bbcard_name a").text
-    projects["#{title}":]={
-      image_link: kickstarter.css(".project-thumbnail a img").attribute("src").value
-      description: kickstarter.css("p.bbcard_blurb").text
-      location: kickstarter.css(".project-meta .location-name").text
-      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-            }
 
+  page_section.each do |project|
+      projects[project]={}
+            # title=project.css(".project .bbcard_name a").text
+            # projects["#{title}":]={
+            #   image_link: project.css(".project-thumbnail a img").attribute("src").value
+            #   description: project.css("p.bbcard_blurb").text
+            #   location: project.css(".project-meta .location-name").text
+            #   percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
   end
   # binding.pry
