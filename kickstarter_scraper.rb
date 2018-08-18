79a78d57f426d8c852956f5ecf8a3891d65a8fb5
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-18 23:43:21 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,15 +7,18 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   project={}
 
-  # page_section = kickstarter.css("li.project.grid_4")
-  # page_section.each do |project|
-  #   title: kickstarter.css(".project .bbcard_name a").text
-  #   image_link: kickstarter.css(".project-thumbnail a img").attribute("src").value
-  #   description: kickstarter.css("p.bbcard_blurb").text
-  #   location: kickstarter.css(".project-meta .location-name").text
-  #   percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-  #
-  # end
+  page_section = kickstarter.css("li.project.grid_4")
+  page_section.each do |project|
+    title=kickstarter.css(".project .bbcard_name a").text
+    project["#{title}":]={
+      image_link: kickstarter.css(".project-thumbnail a img").attribute("src").value
+      description: kickstarter.css("p.bbcard_blurb").text
+      location: kickstarter.css(".project-meta .location-name").text
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+            }
+    
+  
+  end
   # binding.pry
 end
 
